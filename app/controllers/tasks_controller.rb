98c5c7c99123c1f35cef726c6fec3f51713523db
mitsunari matsuzaki <ae15matsuzaki1@gmail.com>
2017-04-24 07:36:39 +0000

    reject

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
  before_action :require_user_logged_in
+ before_action :correct_user, only: [:destroy]
  before_action :set_task, only: [:show, :edit, :update, :destroy]
- 
+
  def index
 	    @tasks = Task.order(created_at: :desc).page(params[:page]).per(5)
  end
